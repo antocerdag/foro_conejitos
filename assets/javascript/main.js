27bf3aee8041726df873a7fe6cd372331b298ab9
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-28 10:57:01 -0400

    ejercicio listo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,11 @@
-main.js
+$(document).ready(function(){
+	$("button").click(function(e){
+		var name = $("#name").val();
+		var message = $("#mensaje").val();
+		if (name == "" && message == "") {
+			alert("Debes escribir tu nombre y el mensaje");
+		}else{
+			$("#comentarios").append('<div><span class="center-align">' + name +'</span></p class="center-align">' + message + '</p></div>');
+		}
+	})
+})
